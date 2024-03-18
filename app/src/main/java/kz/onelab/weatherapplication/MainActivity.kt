a19d3ab439b8f24c3cab34a49707b367d9f7021c
amiralimbekerov <asuraaka@outlook.com>
2024-03-18 14:50:03 +0500

    translate code to fragment

--- app/src/main/java/kz/onelab/weatherapplication/MainActivity.kt
+++ app/src/main/java/kz/onelab/weatherapplication/MainActivity.kt
@@ -1,93 +1,17 @@
 package kz.onelab.weatherapplication
 
-import android.annotation.SuppressLint
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.core.view.isVisible
-import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
-import java.text.SimpleDateFormat
-import androidx.activity.viewModels
 import kz.onelab.weatherapplication.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModels()
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.current.setOnClickListener {
-            if (!binding.cityInput.text.isNullOrBlank()) {
-                binding.weatherBox.isVisible = false
-                binding.root.hideKeyboard()
-                viewModel.getCurrentWeather(binding.cityInput.text.toString())
-            }
-            else Toast.makeText(this, "Input city name", Toast.LENGTH_SHORT).show()
-        }
-        setUpData()
-        setUpLoader()
-        setUpError()
     }
-
-    private fun setUpError() {
-        viewModel.exceptionLiveData.observe(this) {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun setUpLoader() {
-        viewModel.loadingLiveData.observe(this) {
-            binding.loading.isVisible = it
-        }
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun setUpData() {
-        viewModel.currentWeatherLiveData.observe(this) {
-            with(binding) {
-                weatherBox.isVisible = it != null
-
-                city.text = "City: ${it?.location?.name}"
-                region.text = "Region: ${it?.location?.region}"
-                contry.text = "Country: ${it?.location?.country}"
-                coors.text = "Coors: ${it?.location?.lat}, ${it?.location?.lon}"
-                timeZone.text = "Time zone: ${it?.location?.timeZone}"
-                localTime.text = "Time: ${it?.location?.localtime?.getDateTime()}"
-
-                lastUpdate.text = "Last update: ${it?.current?.lastUpdate?.getDateTime()}"
-                temp.text = "Temperature: ${it?.current?.temp}"
-                isDay.text = "Is day: ${if (it?.current?.isDay == 1) "true" else "false"}"
-                wind.text = "Wind: ${it?.current?.wind}"
-                windDegree.text = "Wind degree: ${it?.current?.windDegree}"
-                windDirection.text = "Wind direction: ${it?.current?.windDirection}"
-                conditionText.text = it?.current?.condition?.text
-
-                Glide.with(this@MainActivity)
-                    .load("https:${it?.current?.condition?.icon}")
-                    .into(conditionIcon)
-            }
-        }
-    }
-}
-
-@SuppressLint("SimpleDateFormat")
-fun String.getDateTime(): String? {
-    val fromFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm")
-    val toFormatter = SimpleDateFormat("HH:mm, dd MMMM yyy")
-    return fromFormatter.parse(this)?.let {
-        toFormatter.format(it)
-    }
-}
-
-fun View.hideKeyboard() {
-    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(this.windowToken, 0)
 }
--- app/src/main/java/kz/onelab/weatherapplication/WeatherInfoFragment.kt
+++ app/src/main/java/kz/onelab/weatherapplication/WeatherInfoFragment.kt
@@ -1,22 +1,26 @@
 package kz.onelab.weatherapplication
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
-import kz.onelab.weatherapplication.adapter.WeatherAdapter
-import kz.onelab.weatherapplication.api.WeatherResponse
 import kz.onelab.weatherapplication.databinding.FragmentWeatherInfoBinding
+import java.text.SimpleDateFormat
 
 @AndroidEntryPoint
 class WeatherInfoFragment : Fragment() {
     private lateinit var binding: FragmentWeatherInfoBinding
     private lateinit var viewModel: MainViewModel
-    private var list = emptyList<WeatherResponse>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,13 +31,75 @@ class WeatherInfoFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        binding.current.setOnClickListener {
+            if (!binding.cityInput.text.isNullOrBlank()) {
+                binding.weatherBox.isVisible = false
+                binding.root.hideKeyboard()
+                viewModel.getCurrentWeather(binding.cityInput.text.toString())
+            }
+            else Toast.makeText(this.context, "Input city name", Toast.LENGTH_SHORT).show()
+        }
+        setUpData()
+        setUpLoader()
+        setUpError()
 
         return binding.root
     }
 
-    override fun onDestroy() {
-        list = emptyList()
-        super.onDestroy()
+
+    private fun setUpError() {
+        viewModel.exceptionLiveData.observe(viewLifecycleOwner) {
+            Toast.makeText(this.context, it, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setUpLoader() {
+        viewModel.loadingLiveData.observe(viewLifecycleOwner) {
+            binding.loading.isVisible = it
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setUpData() {
+        viewModel.currentWeatherLiveData.observe(viewLifecycleOwner) {
+            with(binding) {
+                weatherBox.isVisible = it != null
+
+                city.text = "City: ${it?.location?.name}"
+                region.text = "Region: ${it?.location?.region}"
+                contry.text = "Country: ${it?.location?.country}"
+                coors.text = "Coors: ${it?.location?.lat}, ${it?.location?.lon}"
+                timeZone.text = "Time zone: ${it?.location?.timeZone}"
+                localTime.text = "Time: ${it?.location?.localtime?.getDateTime()}"
+
+                lastUpdate.text = "Last update: ${it?.current?.lastUpdate?.getDateTime()}"
+                temp.text = "Temperature: ${it?.current?.temp}"
+                isDay.text = "Is day: ${if (it?.current?.isDay == 1) "true" else "false"}"
+                wind.text = "Wind: ${it?.current?.wind}"
+                windDegree.text = "Wind degree: ${it?.current?.windDegree}"
+                windDirection.text = "Wind direction: ${it?.current?.windDirection}"
+                conditionText.text = it?.current?.condition?.text
+
+                Glide.with(this@WeatherInfoFragment)
+                    .load("https:${it?.current?.condition?.icon}")
+                    .into(conditionIcon)
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun String.getDateTime(): String? {
+        val fromFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm")
+        val toFormatter = SimpleDateFormat("HH:mm, dd MMMM yyy")
+        return fromFormatter.parse(this)?.let {
+            toFormatter.format(it)
+        }
+    }
+
+    private fun View.hideKeyboard() {
+        val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(this.windowToken, 0)
     }
 
 }
+
