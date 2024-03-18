611bebff1a7bd3869e606ef7e866f29e43a66d67
amiralimbekerov <asuraaka@outlook.com>
2024-03-18 14:38:39 +0500

    add AndroidEntryPoint annotation

--- app/src/main/java/kz/onelab/weatherapplication/WeatherInfoFragment.kt
+++ app/src/main/java/kz/onelab/weatherapplication/WeatherInfoFragment.kt
@@ -7,11 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.AndroidEntryPoint
 import kz.onelab.weatherapplication.adapter.WeatherAdapter
 import kz.onelab.weatherapplication.api.WeatherResponse
 import kz.onelab.weatherapplication.databinding.FragmentWeatherInfoBinding
 
-
+@AndroidEntryPoint
 class WeatherInfoFragment : Fragment() {
     private lateinit var binding: FragmentWeatherInfoBinding
     private lateinit var viewModel: MainViewModel
@@ -26,16 +27,7 @@ class WeatherInfoFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        val recyclerView = binding.weatherRecyclerView
-        val adapter = WeatherAdapter()
-        recyclerView.adapter = adapter
 
-        viewModel.currentWeatherLiveData.observe(viewLifecycleOwner) { weatherResponse ->
-            weatherResponse?.let {
-                list = list.toMutableList().apply { add(it) }
-                adapter.submitList(list)
-            }
-        }
         return binding.root
     }
 
