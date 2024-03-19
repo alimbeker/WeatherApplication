c3aa9baa038103e430c78422861aba7fd5a54329
amiralimbekerov <asuraaka@outlook.com>
2024-03-19 23:21:40 +0500

    change api key

--- app/src/main/java/kz/onelab/weatherapplication/api/WeatherApi.kt
+++ app/src/main/java/kz/onelab/weatherapplication/api/WeatherApi.kt
@@ -1,14 +1,20 @@
 package kz.onelab.weatherapplication.api
 
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("current.json?key=bf4dc83bbe114579817143928230611")
+    @GET("current.json?key=9756abb3465d414db22182052241903")
     suspend fun getCurrentWeather(
         @Query("q") city: String,
         @Query("lang") language: String
     ): Response<WeatherResponse>
+    @GET("current.json?key=bf4dc83bbe114579817143928230611")
+    fun getAllData(
+
+    ): Call<List<WeatherResponse>>
+
 }
