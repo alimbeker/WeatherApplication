466819ea54e6a5226ea32cb6ba33df3abbc45be6
amiralimbekerov <asuraaka@outlook.com>
2024-03-20 19:57:01 +0500

    add interceptor

--- app/src/main/java/kz/onelab/weatherapplication/module/NetworkModule.kt
+++ app/src/main/java/kz/onelab/weatherapplication/module/NetworkModule.kt
@@ -5,6 +5,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kz.onelab.weatherapplication.api.WeatherApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -19,8 +21,18 @@ object NetworkModule {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
+
+    // API response interceptor
+    val loggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    // Client
+    val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     @Provides
     @Singleton
