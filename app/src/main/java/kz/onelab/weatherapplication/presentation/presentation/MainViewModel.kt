e001470f111e2d59b8d7cc32490aeb852a88d1bb
amiralimbekerov <asuraaka@outlook.com>
2024-03-23 20:18:22 +0500

    delete BaseViewModel

--- app/src/main/java/kz/onelab/weatherapplication/presentation/presentation/MainViewModel.kt
+++ app/src/main/java/kz/onelab/weatherapplication/presentation/presentation/MainViewModel.kt
@@ -44,7 +44,6 @@ class MainViewModel @Inject constructor(
         }
     }
 
-
     fun getAllData(city: String) {
         _weatherListLiveData.value = Resource.Loading
         viewModelScope.launch {
@@ -57,33 +56,5 @@ class MainViewModel @Inject constructor(
                 }
         }
     }
-
-
 }
 
-abstract class BaseViewModel : ViewModel() {
-    private val coroutineScope = CoroutineScope(Dispatchers.IO + Job())
-
-    private var _loadingLiveData = MutableLiveData<Boolean>()
-    val loadingLiveData: LiveData<Boolean> = _loadingLiveData
-
-    private var _exceptionLiveData = MutableLiveData<String?>()
-    val exceptionLiveData: LiveData<String?> = _exceptionLiveData
-
-    fun <T> launch(
-        request: suspend () -> T, onSuccess: (T) -> Unit = { }
-    ) {
-        coroutineScope.launch {
-            try {
-                _loadingLiveData.postValue(true)
-                val response = request.invoke()
-                onSuccess.invoke(response)
-            } catch (e: Exception) {
-                _exceptionLiveData.postValue(e.message)
-                Log.e(">>>", e.message.orEmpty())
-            } finally {
-                _loadingLiveData.postValue(false)
-            }
-        }
-    }
-}
